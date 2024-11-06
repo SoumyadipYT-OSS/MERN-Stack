e77a52a4179e7f516399d73bda7b39a653b58b75
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-11-07 05:24:28 +0530

    MongoDB is connected and the application backend part is running properly

--- mern-estate/api/index.js
+++ mern-estate/api/index.js
@@ -6,6 +6,7 @@ dotenv.config();
 mongoose.connect(process.env.MONGO).then(() => {
     console.log('Connected to MongoDB!');
 }).catch((err) => {
+    console.log('Connection timeout due to internet high ping!!!');
     console.log(err);
 });
 
