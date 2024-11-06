86514925f954d65af18301572a2eda58cff38aa3
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-11-07 02:56:23 +0530

    Link dependency and Link for the Navbar in App.jsx is added

--- mern-estate/client/src/App.jsx
+++ mern-estate/client/src/App.jsx
@@ -4,9 +4,11 @@ import Profile from './pages/Profile';
 import About from './pages/About';
 import Home from './pages/Home';
 import Signup from './pages/Signup';
+import Header from './components/Header';
 
 export default function App() {
   return <BrowserRouter>
+  <Header />
   <Routes>
     <Route path='/' element={<Home />} />
     <Route path='/sign-in' element={<Signin />} />
